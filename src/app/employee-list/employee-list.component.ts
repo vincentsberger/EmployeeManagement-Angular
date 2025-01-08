1f794a2df4175570f9bd0d077a0c10064174f05b
Vincent Simonsberger <vincent.simonsberger@wamoco.de>
2025-01-08 10:25:14 +0100

    EMS-7: created MainView component

--- src/app/employee-list/employee-list.component.ts
+++ src/app/employee-list/employee-list.component.ts
@@ -1,18 +1,19 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {Observable, of} from "rxjs";
-import {HttpClient, HttpHeaders} from "@angular/common/http";
-import {Employee} from "../Employee";
+import { Observable, of } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Employee } from '../Employee';
 
 @Component({
   selector: 'app-employee-list',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './employee-list.component.html',
-  styleUrl: './employee-list.component.css'
+  styleUrl: './employee-list.component.css',
 })
 export class EmployeeListComponent {
-  bearer = 'eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICIzUFQ0dldiNno5MnlQWk1EWnBqT1U0RjFVN0lwNi1ELUlqQWVGczJPbGU0In0.eyJleHAiOjE3MzQ1MjMwNTAsImlhdCI6MTczNDUxOTQ1MCwianRpIjoiZGY3MGQxOGUtZDc3Zi00MzJjLWI0Y2QtNmYwN2Y1YmI3NmZmIiwiaXNzIjoiaHR0cHM6Ly9rZXljbG9hay5zenV0LmRldi9hdXRoL3JlYWxtcy9zenV0IiwiYXVkIjoiYWNjb3VudCIsInN1YiI6IjU1NDZjZDIxLTk4NTQtNDMyZi1hNDY3LTRkZTNlZWRmNTg4OSIsInR5cCI6IkJlYXJlciIsImF6cCI6ImVtcGxveWVlLW1hbmFnZW1lbnQtc2VydmljZSIsInNlc3Npb25fc3RhdGUiOiI0YmJmNzA4MS0yOTdmLTQzN2UtYjNhZS05ZGQxMjU2NDRjNDkiLCJhY3IiOiIxIiwiYWxsb3dlZC1vcmlnaW5zIjpbImh0dHA6Ly9sb2NhbGhvc3Q6NDIwMCJdLCJyZWFsbV9hY2Nlc3MiOnsicm9sZXMiOlsicHJvZHVjdF9vd25lciIsIm9mZmxpbmVfYWNjZXNzIiwiZGVmYXVsdC1yb2xlcy1zenV0IiwidW1hX2F1dGhvcml6YXRpb24iLCJ1c2VyIl19LCJyZXNvdXJjZV9hY2Nlc3MiOnsiYWNjb3VudCI6eyJyb2xlcyI6WyJtYW5hZ2UtYWNjb3VudCIsIm1hbmFnZS1hY2NvdW50LWxpbmtzIiwidmlldy1wcm9maWxlIl19fSwic2NvcGUiOiJlbWFpbCBwcm9maWxlIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsInByZWZlcnJlZF91c2VybmFtZSI6InVzZXIifQ.JVCE6hYlaeRsJTvy2ibHYlKLcsRoiCxHrgBjZeXGtGvmEX4D761gOvCPntRvwPKLshpps5c0EeqC08oy7CMvVRyXjhHU0zlyk0Q3Kc4R1WjLKvd8IDAGfVZSw4E2iuhw1mS6CyvsQftfxPF4vWAzUYfCNA5ZZZPcLTfYgwZFUm6tUq53ECD-5VDNRdsjyk6_JJHFZJptrr2UADgRZmuZXwFa_uaas8PcoR7YOEJ9dtIHr3zq4h11sL2PmodEd2WaMTCImgR0qEdd3krSxisdCWbqOCe2nUY4swPfr1CE9L3v3YUGJ7_In-zN054agZ4gU2bbZQFoIBkPe92S99tz3A';
+  bearer =
+    'eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICIzUFQ0dldiNno5MnlQWk1EWnBqT1U0RjFVN0lwNi1ELUlqQWVGczJPbGU0In0.eyJleHAiOjE3MzYzMjU4NzksImlhdCI6MTczNjMyMjI3OSwianRpIjoiZmY5MjY2OTEtNTZmMy00ZmE1LThjOGYtM2YwNTY2ZTBhNjRiIiwiaXNzIjoiaHR0cHM6Ly9rZXljbG9hay5zenV0LmRldi9hdXRoL3JlYWxtcy9zenV0IiwiYXVkIjoiYWNjb3VudCIsInN1YiI6IjU1NDZjZDIxLTk4NTQtNDMyZi1hNDY3LTRkZTNlZWRmNTg4OSIsInR5cCI6IkJlYXJlciIsImF6cCI6ImVtcGxveWVlLW1hbmFnZW1lbnQtc2VydmljZSIsInNlc3Npb25fc3RhdGUiOiIxYzIzMDU2Yi1lM2NkLTQ5YmItYWY3NS05ZDQ0MTk4YzU2ZWQiLCJhY3IiOiIxIiwiYWxsb3dlZC1vcmlnaW5zIjpbImh0dHA6Ly9sb2NhbGhvc3Q6NDIwMCJdLCJyZWFsbV9hY2Nlc3MiOnsicm9sZXMiOlsicHJvZHVjdF9vd25lciIsIm9mZmxpbmVfYWNjZXNzIiwiZGVmYXVsdC1yb2xlcy1zenV0IiwidW1hX2F1dGhvcml6YXRpb24iLCJ1c2VyIl19LCJyZXNvdXJjZV9hY2Nlc3MiOnsiYWNjb3VudCI6eyJyb2xlcyI6WyJtYW5hZ2UtYWNjb3VudCIsIm1hbmFnZS1hY2NvdW50LWxpbmtzIiwidmlldy1wcm9maWxlIl19fSwic2NvcGUiOiJlbWFpbCBwcm9maWxlIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsInByZWZlcnJlZF91c2VybmFtZSI6InVzZXIifQ.Z1gdh5sWgsfJF8hwIjHQdSBqQZPgbWICjTAjD-w3a1NI-qEZVQQegLHCbJRAtIpPfGxUV9EE0btDrOkLM0Y5G6O6TCh6-hcv_2XDPgn7qbdHvrIT72b4QyUJxsvj20zTvg-13H-q9x3fNh06fhaKGIPga-V8X06wtsbLCtcrTchbHZTVlcz18IB4-P50NRkpBL-Ld0hm5han2JjaZSy5cXiWYpLlTnDG3cxEXIT_9p_oMn9w6auDbtiUVYqjIiXdQ6I2WO8uI4go7lHz_seW80HB2y01tvSnCRnR5wiehclqrkltlluYGoXSEchrgDTPig9L39SrpYPO0H8vW7sRBg';
   employees$: Observable<Employee[]>;
 
   constructor(private http: HttpClient) {
@@ -21,10 +22,13 @@ export class EmployeeListComponent {
   }
 
   fetchData() {
-    this.employees$ = this.http.get<Employee[]>('http://localhost:8089/employees', {
-      headers: new HttpHeaders()
-        .set('Content-Type', 'application/json')
-        .set('Authorization', `Bearer ${this.bearer}`)
-    });
+    this.employees$ = this.http.get<Employee[]>(
+      'http://localhost:8089/employees',
+      {
+        headers: new HttpHeaders()
+          .set('Content-Type', 'application/json')
+          .set('Authorization', `Bearer ${this.bearer}`),
+      }
+    );
   }
 }
