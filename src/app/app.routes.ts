1b91443d0acc55efcdba068e83850e9a05e2916b
Manuel Gilles <117361287+manug243@users.noreply.github.com>
2025-01-15 11:08:22 +0100

    feat: created AuthGuard and added routing + silent sso check

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { AuthGuard } from './auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component:  },
+  {
+    path: '',
+    component: ,
+    canActivate: [AuthGuard],
+  },
+];
---
+++ src/app/auth.guard.ts
@@ -0,0 +1,19 @@
+import {Injectable} from '@angular/core';
+import {CanActivate, Router} from '@angular/router';
+import {KeycloakService} from 'keycloak-angular';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class AuthGuard implements CanActivate {
+    constructor(private keycloakService: KeycloakService, private router: Router) {
+    }
+
+    async canActivate(): Promise<boolean> {
+        const isLoggedIn = await this.keycloakService.isLoggedIn();
+        if (isLoggedIn) return true;
+
+        await this.keycloakService.login();
+        return false;
+    }
+}
