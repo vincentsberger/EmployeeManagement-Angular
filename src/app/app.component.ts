c0fd234c5bb827147f04e2990d076da27d529946
Vincent Simonsberger <vincent.simonsberger@wamoco.de>
2025-01-15 09:21:18 +0100

    EMS-6: base config, main view component, router config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
-import {EmployeeListComponent} from "./employee-list/employee-list.component";
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { MainViewComponent } from './components/main-view/main-view.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, EmployeeListComponent],
+  imports: [CommonModule, MainViewComponent, RouterOutlet, RouterLinkActive, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'lf10StarterNew';
+  title = 'EmployeeManagementService';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: "", component: EmployeeListComponent },
+  { path: "employees", component: EmployeeListComponent },
+  { path: "qualifications", component: EmployeeListComponent}
+];
