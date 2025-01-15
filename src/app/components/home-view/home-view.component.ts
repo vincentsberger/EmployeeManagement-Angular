39c8294ac112efa14e0703b0fb5a16c21e8656e2
Vincent Simonsberger <vincent.simonsberger@wamoco.de>
2025-01-15 11:31:47 +0100

    EMS-6: working on home page content, added home view route

---
+++ src/app/components/home-view/home-view.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-home-view',
+  standalone: true,
+  imports: [MatIconModule],
+  templateUrl: './home-view.component.html',
+  styleUrl: './home-view.component.css'
+})
+export class HomeViewComponent {
+
+}
