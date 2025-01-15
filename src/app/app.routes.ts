45886a54df3d5745a46fc32ee8fd320213aa60a5
Manuel Gilles <117361287+manug243@users.noreply.github.com>
2025-01-15 11:08:22 +0100

    feat: created AuthGuard and added routing + silent sso check

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,15 @@
 import { Routes } from '@angular/router';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
 import { HomeViewComponent } from './components/home-view/home-view.component';
+import { AuthGuard } from './auth.guard';
 
 export const routes: Routes = [
+  {
+    path: '',
+    component: HomeViewComponent,
+    canActivate: [AuthGuard],
+  },
+
   { path: "", component: HomeViewComponent },
   { path: "employees", component: EmployeeListComponent },
   { path: "qualifications", component: EmployeeListComponent}
