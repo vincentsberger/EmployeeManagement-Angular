7e04a048f8a113d255c371d3b62cf33f515ed0f3
Vincent Simonsberger <vincent.simonsberger@wamoco.de>
2025-01-16 09:43:01 +0100

    EMS-6: finished home view

--- src/app/components/home-view/home-view.component.ts
+++ src/app/components/home-view/home-view.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { MatGridListModule } from "@angular/material/grid-list";
 
 @Component({
   selector: 'app-home-view',
   standalone: true,
-  imports: [MatIconModule],
+  imports: [MatIconModule, MatGridListModule],
   templateUrl: './home-view.component.html',
   styleUrl: './home-view.component.css'
 })
