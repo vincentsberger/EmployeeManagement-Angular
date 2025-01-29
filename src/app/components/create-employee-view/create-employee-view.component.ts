022a250bb3a142b2fabc6d8169decf890f1f2dd9
Martin Sander <martinsander88@gmail.com>
2025-01-29 11:02:27 +0100

    Qualifications/Skills are now selectable in a material design multiselect list. Fetching the Data from the Database is not functional. The Options are static atm for testing

--- src/app/components/create-employee-view/create-employee-view.component.ts
+++ src/app/components/create-employee-view/create-employee-view.component.ts
@@ -1,25 +1,61 @@
-import {Component, inject} from '@angular/core';
+import {Component, inject, ViewChild} from '@angular/core';
 import {MainViewComponent} from "../main-view/main-view.component";
 import {RouterLink} from "@angular/router";
-import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
 import {HttpClient, HttpHeaders} from "@angular/common/http";
 import Keycloak from "keycloak-js";
+import {QualificationViewComponent} from "../qualification-view/qualification-view.component";
+import {Qualification} from "../../model/Qualification";
+import {AsyncPipe, NgForOf} from "@angular/common";
+import {MatFormField, MatFormFieldModule} from "@angular/material/form-field";
+import {MatOption, MatSelect, MatSelectModule} from "@angular/material/select";
+import {BrowserModule} from "@angular/platform-browser";
+import {MatOptionModule} from "@angular/material/core";
+import {MatInputModule} from "@angular/material/input";
+import {MatIconModule} from "@angular/material/icon";
 
 @Component({
   selector: 'app-create-employee-view',
   imports: [
     MainViewComponent,
     RouterLink,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    QualificationViewComponent,
+    FormsModule,
+    AsyncPipe,
+    NgForOf,
+    MatFormField,
+    MatSelect,
+    MatOption,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatOptionModule,
+    MatInputModule,
+    MatIconModule,
+    FormsModule
   ],
+
   templateUrl: './create-employee-view.component.html',
   styleUrl: './create-employee-view.component.scss'
 })
+
+
 export class CreateEmployeeViewComponent {
   keycloak = inject(Keycloak);
   bearer = this.keycloak.token;
-
   employeeForm: FormGroup;
+
+  options = ['Apple', 'Banana', 'Cherry', 'Date', 'Elderberry'];
+  selectedItems: string[] = [];
+  searchQuery: string = '';
+  filteredOptions = [...this.options]; // Initialize with all options
+
+
+  // filterOptions() {
+  //   this.filteredOptions = this.options.filter(option =>
+  //     option.toLowerCase().includes(this.searchQuery.toLowerCase())
+  //   );
+  // }
 
   constructor(private fb: FormBuilder, private http: HttpClient) {
     this.employeeForm = this.fb.group({
@@ -29,7 +65,7 @@ export class CreateEmployeeViewComponent {
       postcode: ['', Validators.required],
       city: ['', Validators.required],
       phone: ['', Validators.required],
-      skillSet:[0]
+      skillSet: [0, Validators.required],
     });
   }
 
@@ -40,11 +76,11 @@ export class CreateEmployeeViewComponent {
 
       // Send the data to the API
       this.http.post('http://localhost:8089/employees', {
-        headers: new HttpHeaders()
-          .set('Content-Type', 'application/json')
-          .set('Authorization', `Bearer ${this.bearer}`),
-      }
-      , formData)
+          headers: new HttpHeaders()
+            .set('Content-Type', 'application/json')
+            .set('Authorization', `Bearer ${this.bearer}`),
+        }
+        , formData)
         .subscribe({
           next: (response) => {
             console.log('Employee data saved successfully!', response);
@@ -57,4 +93,7 @@ export class CreateEmployeeViewComponent {
       console.warn('Form is invalid. Please check the fields.');
     }
   }
+
+  protected readonly QualificationViewComponent = QualificationViewComponent;
+  protected readonly Qualification = Qualification;
 }
