ca1ad72354db68b2044d6323ee6a9799cefcf91d
Manuel Gilles <117361287+manug243@users.noreply.github.com>
2025-01-30 17:57:44 +0100

    feat: adding new skills and assign them to employee, cleaned up html and scss and optimized code

--- src/app/components/create-employee-view/create-employee-view.component.ts
+++ src/app/components/create-employee-view/create-employee-view.component.ts
@@ -1,106 +1,140 @@
 import {Component, inject} from '@angular/core';
-import {MainViewComponent} from "../main-view/main-view.component";
-import {RouterLink} from "@angular/router";
-import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
-import {HttpClient, HttpHeaders} from "@angular/common/http";
-import Keycloak from "keycloak-js";
-import {QualificationViewComponent} from "../qualification-view/qualification-view.component";
-import {Qualification} from "../../model/Qualification";
-import {AsyncPipe, NgForOf} from "@angular/common";
-import {MatFormField, MatFormFieldModule} from "@angular/material/form-field";
-import {MatOption, MatSelect, MatSelectModule} from "@angular/material/select";
-import {MatOptionModule} from "@angular/material/core";
-import {MatInputModule} from "@angular/material/input";
-import {MatIconModule} from "@angular/material/icon";
-import {Observable, of} from "rxjs";
-import {Router} from "@angular/router";
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Router} from '@angular/router';
+import Keycloak from 'keycloak-js';
+import {Qualification} from '../../model/Qualification';
+import {BehaviorSubject, Observable} from 'rxjs';
+import {MainViewComponent} from '../main-view/main-view.component';
+import {MatOption, MatFormField, MatSelect} from '@angular/material/select';
+import {AsyncPipe, NgForOf, NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-create-employee-view',
-  imports: [
-    MainViewComponent,
-    RouterLink,
-    ReactiveFormsModule,
-    FormsModule,
-    NgForOf,
-    MatFormField,
-    MatSelect,
-    MatOption,
-    MatFormFieldModule,
-    MatSelectModule,
-    MatOptionModule,
-    MatInputModule,
-    MatIconModule,
-    FormsModule,
-    AsyncPipe
-  ],
   templateUrl: './create-employee-view.component.html',
-  styleUrl: './create-employee-view.component.scss'
+  imports: [
+    MatOption,
+    MatSelect,
+    MatFormField,
+    AsyncPipe,
+    NgForOf,
+    ReactiveFormsModule,
+    MainViewComponent,
+    NgIf
+  ],
+  styleUrls: ['./create-employee-view.component.scss']
 })
-
-
 export class CreateEmployeeViewComponent {
   keycloak = inject(Keycloak);
   bearer = this.keycloak.token;
   employeeForm: FormGroup;
-  options$: Observable<Qualification[]>;
-
   selectedItems: number[] = [];
+  qualificationsSubject = new BehaviorSubject<Qualification[]>([]);
+  options$: Observable<Qualification[]> = this.qualificationsSubject.asObservable();
 
   constructor(private fb: FormBuilder, private http: HttpClient, private router: Router) {
-    this.options$ = of([]);
-    this.fetchData();
-
     this.employeeForm = this.fb.group({
-      lastName: ['', Validators.required],
       firstName: ['', Validators.required],
-      street: ['', Validators.required],
-      postcode: ['', Validators.required],
-      city: ['', Validators.required],
-      phone: ['', Validators.required],
+      lastName: ['', Validators.required],
+      phone: [''],
+      street: [''],
+      postcode: ['', [Validators.required, Validators.pattern('^[0-9]{5}$')]],
+      city: [''],
       skillSet: [this.selectedItems],
+      newSkill: ['']
     });
+
+    this.fetchData();
   }
 
-  fetchData() {
-    this.options$ = this.http.get<Qualification[]>('http://localhost:8089/qualifications', {
-      headers: new HttpHeaders()
-        .set('Content-Type', 'application/json')
-        .set('Authorization', `Bearer ${this.bearer}`)
-    });
+  private fetchData() {
+    this.http.get<Qualification[]>('http://localhost:8089/qualifications', {
+      headers: new HttpHeaders().set('Authorization', `Bearer ${this.bearer}`)
+    }).subscribe(qualifications => this.qualificationsSubject.next(qualifications));
+  }
+
+  async addSkill() {
+    const newSkillValue = this.employeeForm.get('newSkill')?.value.trim();
+    if (newSkillValue) {
+      await this.checkIfQualificationExists(newSkillValue);
+    } else {
+      this.resetNewSkill();
+    }
+  }
+
+  private async checkIfQualificationExists(skill: string): Promise<void> {
+    const qualifications = this.qualificationsSubject.getValue();
+    const existingQualification = qualifications.find(q => q.skill!.toLowerCase() === skill.toLowerCase());
+
+    if (existingQualification) {
+      this.addExistingQualificationToEmployee(existingQualification.id!);
+    } else {
+      await this.addQualificationToEmployee(skill);
+    }
+  }
+
+  private addExistingQualificationToEmployee(qualificationId: number): void {
+    this.selectedItems.push(qualificationId);
+    this.updateSkillSet();
+    this.resetNewSkill();
+  }
+
+  private async addQualificationToEmployee(skill: string): Promise<void> {
+    try {
+      const newQualification = await this.http
+        .post<Qualification>('http://localhost:8089/qualifications', {skill: skill.trim()}, {
+          headers: new HttpHeaders().set('Authorization', `Bearer ${this.bearer}`)
+        })
+        .toPromise();
+
+      if (newQualification?.id) {
+        this.qualificationsSubject.next([...this.qualificationsSubject.getValue(), newQualification]);
+        this.selectedItems.push(newQualification.id);
+        this.updateSkillSet();
+      }
+    } catch (error) {
+      this.handleError('Fehler beim Erstellen der Qualifikation.');
+    } finally {
+      this.resetNewSkill();
+    }
+  }
+
+  private updateSkillSet() {
+    this.employeeForm.patchValue({skillSet: this.selectedItems});
+  }
+
+  private resetNewSkill() {
+    this.employeeForm.get('newSkill')?.setValue('');
   }
 
   onSelectionChange(event: any) {
     this.selectedItems = event.value;
-    this.employeeForm.patchValue({ skillSet: this.selectedItems });
+    this.updateSkillSet();
   }
 
-  onSubmit() {
+  async onSubmit() {
     if (this.employeeForm.valid) {
-      // Retrieve form data
-      const formData = this.employeeForm.value;
-
-      // Send the data to the API
-      this.http.post('http://localhost:8089/employees', formData, {
-          headers: new HttpHeaders()
-            .set('Content-Type', 'application/json')
-            .set('Authorization', `Bearer ${this.bearer}`),
-        }).subscribe({
-          next: (response) => {
-            console.log(response);
-            alert('Mitarbeiter Daten erfolgreich gespeichert!')
-            this.router.navigate(['/employees']);
-          },
-          error: (err) => {
-            console.error(err);
-            alert('Fehler beim speichern der Daten')
-          }
-        });
+      try {
+        await this.http
+          .post('http://localhost:8089/employees', this.employeeForm.value, {
+            headers: new HttpHeaders().set('Authorization', `Bearer ${this.bearer}`)
+          })
+          .toPromise();
+        alert('Mitarbeiter erfolgreich gespeichert!');
+        this.router.navigate(['/employees']);
+      } catch (err: unknown) {
+        if (err instanceof Error) {
+          this.handleError(err.message || 'Unbekannter Fehler aufgetreten');
+        } else {
+          this.handleError('Unbekannter Fehler aufgetreten');
+        }
+      }
     } else {
-      alert('Daten fehlerhaft, bitte alle Felder korrekt ausfüllen');
+      alert('Bitte alle Felder korrekt ausfüllen.');
     }
   }
 
-  protected readonly QualificationViewComponent = QualificationViewComponent;
-  protected readonly Qualification = Qualification;
+  private handleError(message: string) {
+    alert(message);
+  }
 }
