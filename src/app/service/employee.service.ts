4b12e819cdbb0728e82a4dff757b1192fceb7052
Vincent Simonsberger <vincent.simonsberger@wamoco.de>
2025-02-02 15:25:01 +0100

    updated qualification service complete, employee service wip

--- src/app/service/employee.service.ts
+++ src/app/service/employee.service.ts
@@ -4,8 +4,11 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import Keycloak from 'keycloak-js';
 import { QualificationEmployeesDTO } from '../model/DTO/qualification-employees-dto';
 import { Qualification } from '../model/Qualification';
-import { map, Observable } from 'rxjs';
+import { BehaviorSubject, map, Observable, tap } from 'rxjs';
 import { PostEmployeeDTO } from '../model/DTO/post-employee-dto';
+import { ApiService } from './api.service';
+import { ApiRoutes } from '../enums/api-routes';
+import { LoggingService } from './logging.service';
 
 @Injectable({
   providedIn: 'root',
@@ -13,20 +16,38 @@ import { PostEmployeeDTO } from '../model/DTO/post-employee-dto';
 export class EmployeeService {
   keycloak = inject(Keycloak);
   bearer = this.keycloak.token;
+  private _employeesSubject = new BehaviorSubject<Employee[]>([]);
+  readonly employees$ = this._employeesSubject.asObservable();
+  protected logger$ = inject(LoggingService);
 
-  constructor(private httpClient: HttpClient) {}
+  constructor(
+    private httpClient: HttpClient,
+    private apiService: ApiService,
+    private loggingService: LoggingService
+  ) {}
 
   /**
    * Retrieves a list of all employees.
    *
    * @returns An observable of a list of employees.
    */
-  public getEmployees(): Observable<Employee[]> {
+  public fetchEmployees(): Observable<Employee[]> {
     return this.httpClient.get<Employee[]>(`http://localhost:8089/employees`, {
       headers: new HttpHeaders()
         .set('Content-Type', 'application/json')
         .set('Authorization', `Bearer ${this.bearer}`),
     });
+  }
+
+  /**
+   * Returns an observable of a list of all employees.
+   * This method simply returns the internal state of the service, which is
+   * populated by the fetchEmployees method when the service is constructed.
+   *
+   * @returns An observable of a list of employees.
+   */
+  public getEmployees(): Observable<Employee[]> {
+    return this.employees$;
   }
 
   /**
@@ -55,17 +76,13 @@ export class EmployeeService {
   public getEmployeesByQualificationId(
     qualificationId: number
   ): Observable<Employee[]> {
-    let qualificationEmployees = this.httpClient.get<QualificationEmployeesDTO>(
-      `http://localhost:8089/qualifications/${qualificationId}/employees`,
-      {
-        headers: new HttpHeaders()
-          .set('Content-Type', 'application/json')
-          .set('Authorization', `Bearer ${this.bearer}`),
-      }
-    );
-    return qualificationEmployees.pipe(
-      map((data: QualificationEmployeesDTO): Employee[] => data.employees)
-    );
+    return this.apiService
+      .sendGetRequest<QualificationEmployeesDTO>(
+        `${ApiRoutes.QUALIFICATIONS}/${qualificationId}/employees`
+      )
+      .pipe(
+        map((data: QualificationEmployeesDTO): Employee[] => data.employees)
+      );
   }
 
   /**
@@ -104,5 +121,27 @@ export class EmployeeService {
           .set('Authorization', `Bearer ${this.bearer}`),
       }
     );
+  }
+
+  /**
+   * Deletes a qualification from a specific employee.
+   *
+   * @param qualification - The qualification to delete from the employee.
+   * @param employee - The employee from which to delete the qualification.
+   * @returns An observable of the deleted qualification.
+   */
+  public removeQualificationFromEmployee(
+    qualification: Qualification,
+    employee: Employee
+  ): Observable<Employee> {
+    return this.apiService
+      .sendDeleteRequest<Employee>(
+        `${ApiRoutes.EMPLOYEES}/${employee.id}/qualifications/${qualification.id}`
+      )
+      .pipe(
+        tap(() =>
+          this.logger$.debug(`Qualifikation aus Mitarbeiter entfernt.`, {qualification, employee})
+        )
+      );
   }
 }
--- src/app/service/qualification.service.ts
+++ src/app/service/qualification.service.ts
@@ -2,125 +2,166 @@ import { inject, Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Qualification } from '../model/Qualification';
 import Keycloak from 'keycloak-js';
-import { Observable } from 'rxjs';
+import {
+  BehaviorSubject,
+  catchError,
+  concatMap,
+  count,
+  finalize,
+  forkJoin,
+  from,
+  Observable,
+  retry,
+  switchMap,
+  tap,
+} from 'rxjs';
 import { Employee } from '../model/Employee';
 import { EmployeeService } from './employee.service';
+import { ApiService } from './api.service';
+import { ApiRoutes } from '../enums/api-routes';
+import { ErrorHandlerService } from './error-handler.service';
+import { LoggingService } from './logging.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class QualificationService {
-  keycloak = inject(Keycloak);
-  bearer = this.keycloak.token;
+  private qualificationsSubject = new BehaviorSubject<Qualification[]>([]);
+  public qualifications$ = this.qualificationsSubject.asObservable();
+  protected logger$ = inject(LoggingService);
 
   constructor(
-    private httpClient: HttpClient,
-    private employeeService: EmployeeService
-  ) {}
+    private employeeService: EmployeeService,
+    private apiService: ApiService
+  ) {
+    this.fetchQualifications();
+  }
 
   /**
-   * Retrieves a list of all qualifications.
+   * Fetches the list of all qualifications from the backend.
    *
-   * @returns An observable of a list of qualifications.
+   * This method sends a GET request to the backend, and updates the
+   * `qualifications$` observable with the response. The response is expected
+   * to be an array of `Qualification` objects.
    */
+  public fetchQualifications(): void {
+    this.apiService
+      .sendGetRequest<Qualification[]>(ApiRoutes.QUALIFICATIONS)
+      .subscribe((data: Qualification[]): void => {
+        this.qualificationsSubject.next(data);
+      });
+  }
+
   public getQualifications(): Observable<Qualification[]> {
-    return this.httpClient.get<Qualification[]>(
-      'http://localhost:8089/qualifications',
-      {
-        headers: new HttpHeaders()
-          .set('Content-Type', 'application/json')
-          .set('Authorization', `Bearer ${this.bearer}`),
-      }
-    );
+    return this.qualifications$;
   }
 
   /**
    * Retrieves a single qualification by its ID.
    *
    * @param qualificationId - The ID of the qualification to retrieve.
-   * @returns An observable of the qualification.
+   * @returns An observable of the retrieved qualification.
    */
-  public getQualificationById(qualificationId: number) {
-    return this.httpClient.get<Qualification>(
-      `http://localhost:8089/qualifications/${qualificationId}`,
-      {
-        headers: new HttpHeaders()
-          .set('Content-Type', 'application/json')
-          .set('Authorization', `Bearer ${this.bearer}`),
-      }
+  public getQualificationById(
+    qualificationId: number
+  ): Observable<Qualification> {
+    return this.apiService.sendGetRequest<Qualification>(
+      `${ApiRoutes.QUALIFICATIONS}/${qualificationId}`
     );
   }
 
   /**
-   * Creates a new qualification and adds it to the database.
+   * Adds a new qualification to the backend.
    *
-   * @param qualification - The qualification to add.
-   * @returns An observable of the newly created qualification.
+   * This method sends a POST request to the backend with the provided
+   * `postBody`, and updates the `qualifications$` observable with the
+   * response. The response is expected to be a single `Qualification`
+   * object.
+   *
+   * @param newQualification - The `Qualification` object that should be added.
+   * @param postBody - The body of the POST request.
    */
-  public addQualification(qualification: Qualification) {
-    return this.httpClient.post<Qualification>(
-      `http://localhost:8089/qualifications`,
-      qualification,
-      {
-        headers: new HttpHeaders()
-          .set('Content-Type', 'application/json')
-          .set('Authorization', `Bearer ${this.bearer}`),
-      }
-    );
+  public addQualification(newQualification: Qualification): void {
+    this.apiService
+      .sendPostRequest<Qualification>(
+        ApiRoutes.QUALIFICATIONS,
+        newQualification
+      )
+      .subscribe((qualification: Qualification): void => {
+        this.logger$.debug(
+          'Qualifikation erfolgreich hinzugefügt!',
+          qualification
+        );
+        this.fetchQualifications();
+      });
   }
 
   public updateQualification(qualification: Qualification) {}
 
-  /**
-   * Deletes a qualification from the database and removes it from all associated employees.
-   *
-   * This method first retrieves all employees associated with the given qualification.
-   * If there are any employees with this qualification, it removes the qualification from each employee.
-   * After a brief delay, it deletes the qualification from the database.
-   *
-   * @param qualification - The qualification to be deleted.
-   */
-  public deleteQualification(qualification: Qualification) {
-    let qualificationEmployees$ =
-      this.employeeService.getEmployeesByQualificationId(qualification.id);
-    qualificationEmployees$.subscribe({
-      next: (employeeCollection: Employee[]) => {
-        if (employeeCollection.length == 0) {
-          return;
-        }
-        employeeCollection.forEach((employee) => {
-          this.deleteQualficationfromEmployee(qualification, employee);
-        });
-      },
-    });
-    return this.httpClient.delete<Qualification>(
-      `http://localhost:8089/qualifications/${qualification.id}`,
-      {
-        headers: new HttpHeaders()
-          .set('Content-Type', 'application/json')
-          .set('Authorization', `Bearer ${this.bearer}`),
-      }
-    );
-  }
 
   /**
-   * Deletes a qualification from a specific employee.
+   * Deletes a qualification from the backend.
    *
-   * @param qualification - The qualification to delete from the employee.
-   * @param employee - The employee from which to delete the qualification.
-   * @returns An observable of the deleted qualification.
+   * This method first checks if the qualification is associated with any
+   * employees. If it is not, the qualification is deleted directly. If it is,
+   * the method first removes the qualification from all associated employees,
+   * and then deletes the qualification.
+   *
+   * @param qualification - The `Qualification` object that should be deleted.
+   * @returns An observable of the deleted `Qualification`.
    */
-  private deleteQualficationfromEmployee(
-    qualification: Qualification,
-    employee: Employee
-  ) {
-    return this.httpClient.delete<Qualification>(
-      `http://localhost:8089/employees/${employee.id}/qualifications/${qualification.id}`,
-      {
-        headers: new HttpHeaders()
-          .set('Content-Type', 'application/json')
-          .set('Authorization', `Bearer ${this.bearer}`),
-      }
-    );
+  public deleteQualification(
+    qualification: Qualification
+  ): Observable<Qualification> | any {
+    // Schritt 1: Überprüfen, ob die Qualifikation Mitarbeitern zugeordnet ist
+    return this.employeeService
+      .getEmployeesByQualificationId(qualification.id)
+      .pipe(
+        switchMap((employees: Employee[]): Observable<Qualification> => {
+          if (employees.length === 0) {
+            // Keine zugeordneten Mitarbeiter → Direkt löschen
+            return this.apiService
+              .sendDeleteRequest<Qualification>(
+                `${ApiRoutes.QUALIFICATIONS}/${qualification.id}`
+              )
+              .pipe(
+                tap((): void =>
+                  this.logger$.debug(
+                    `Qualifikation ${qualification.skill}[${qualification.id}] erfolgreich gelöscht!`,
+                    qualification
+                  )
+                )
+              );
+          } else {
+            // Mitarbeiter-Qualifikationen zuerst entfernen
+            const deleteRequests = employees.map(
+              (employee): Observable<Employee> =>
+                this.employeeService.removeQualificationFromEmployee(
+                  qualification,
+                  employee
+                )
+            );
+            // Schritt 2: Nachdem alle Mitarbeiter aktualisiert wurden → Qualifikation löschen
+            return forkJoin(deleteRequests).pipe(
+              switchMap(
+                (): Observable<Qualification> =>
+                  this.apiService
+                    .sendDeleteRequest<Qualification>(
+                      `${ApiRoutes.QUALIFICATIONS}/${qualification.id}`
+                    )
+                    .pipe(
+                      tap((): void =>
+                        this.logger$.debug(
+                          `Qualifikation erfolgreich gelöscht!`,
+                          qualification
+                        )
+                      )
+                    )
+              )
+            );
+          }
+        }),
+        finalize(() => this.fetchQualifications())
+      );
   }
 }
